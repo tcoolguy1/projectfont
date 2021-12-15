2f99d62125e9009cb5f0542d710aca2c43c40b51
tcoolguy1 <78965454+tcoolguy1@users.noreply.github.com>
2021-12-15 18:13:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-nosex = 0;
-nosey = 0;
 difference = 0;
 rightWristx = 0;
 leftWristx = 0;
@@ -16,10 +14,6 @@ function gotPoses(results) {
     if(results.length > 0)
     {
         console.log(results);
-         nosex = results[0].pose.nose.x;
-         nosey = results[0].pose.nose.y;
-         console.log(nosex);
-         console.log(nosey);
          leftWristx = results[0].pose.leftWrist.x;
          rightWristx = results[0].pose.rightWrist.x;
          difference = floor( leftWristx - rightWristx);
@@ -33,10 +27,12 @@ function modelLoaded() {
     console.log('PoseNet Is Initialized!');
 }
 
+
 function draw() {
     background('#969A97');
     fill('cyan');
     stroke('green');
-    square(nosex, nosey, difference);
-    document.getElementById("square_side").innerHTML = "sides of the square is " + difference + "px";
+    textSize(difference);
+    text('Uncle', 250, 250);
+    document.getElementById("square_side").innerHTML = "Size of the text " + difference + "px";
 }
